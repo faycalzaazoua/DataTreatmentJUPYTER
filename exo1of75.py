3b751728827741691a8d6a55c639cb1184af721c
faycalzaazoua <zaazoua.faycal1@gmail.com>
2022-09-15 16:24:33 +0200

    work on problem1of75

---
+++ exo1of75.py
@@ -0,0 +1,21 @@
+# EXO75_TwoSum
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dic = [None]*target
+        dic2=[None]*target
+        result=[]
+        c=0
+
+
+        for c,i in enumerate(nums):
+            value= target-i
+            if dic[value] == None:
+                    dic[i] = i
+                    dic2[i] = c
+            else:
+                result.append(dic2[value])
+                result.append(c)
+                return result 
+    
+
+
